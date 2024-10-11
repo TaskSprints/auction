7f18cb21ea44096c4bf05c83fe9b49128bf3d481
임채연 <dlacodus0407@gmail.com>
2024-10-11 17:08:23 +0900

    test : Test expired token and valid token >> >> - verifyToken_valid 메소드로 토큰의 유효성을 검사 >> - verifyToken_expired 메소드로 만료된 토큰에 대해 예외를 발생시키는지 검사

--- src/test/java/com/tasksprints/auction/common/jwt/JwtProviderTest.java
+++ src/test/java/com/tasksprints/auction/common/jwt/JwtProviderTest.java
@@ -1,5 +1,7 @@
 package com.tasksprints.auction.common.jwt;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,5 +32,25 @@ class JwtProviderTest {
     void createAccessToken() {
         String token = jwtProvider.createAccessToken(1L);
         assertNotNull(token, "토큰이 발급되었습니다.");
+    }
+
+    @Test
+    @DisplayName("유효한 토큰 테스트")
+    void verifyToken_valid() {
+        String token = jwtProvider.createAccessToken(1L);
+        Assertions.assertTrue(jwtProvider.verifyToken(token));
+    }
+
+    @Test
+    @DisplayName("만료된 토큰 테스트")
+    void verifyToken_expired() {
+
+        when(jwtProperties.getExpireMs()).thenReturn(0L);
+
+        String token = jwtProvider.createAccessToken(1L);
+
+        Assertions.assertThrows(ExpiredJwtException.class,
+            () -> { jwtProvider.verifyToken(token); },
+            "토큰이 즉시 만료되어야 합니다.");
     }
 }
